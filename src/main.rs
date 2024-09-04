ca286b5fa9a48c682bda1fb19115658d2091b95b
rafacostavilla <rafaelcostavilla@gmail.com>
2024-09-04 17:51:53 +1200

    Use threads and closure together

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
-use std::{thread, time::Duration};
+use std::{thread};
 
-fn main() {
-    let handle = thread::spawn(|| {
-        for i in 1..10{
-            println!("Hi number {} from the spawned thread!", i);
-            thread::sleep(Duration::from_millis(1));
-        }
+fn main(){
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move ||{
+        println!("Here's a vector: {:?}", v);
     });
-    
-    for i in 1..5{
-        println!("Hi number {} from the main thread!", i);
-        thread::sleep(Duration::from_millis(1));
-    }
+
     handle.join().unwrap();
-    
 }
