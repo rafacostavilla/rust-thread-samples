ed3b7cd6b72e936e4a9cf45126bc42af51f3f4cb
rafacostavilla <rafaelcostavilla@gmail.com>
2024-09-04 19:16:02 +1200

    Use channel to send multiple messages

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
-use std::{sync::mpsc, thread};
+use std::{sync::mpsc, thread, time::Duration};
 
 fn main(){
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
-        let msg = String::from("hi");
-        tx.send(msg).unwrap();
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+
     });
 
-    let received = rx.recv().unwrap();
-    println!("Got: {}", received);
+    for received in rx{
+        println!("Got: {}", received);
+    }
+
 }
