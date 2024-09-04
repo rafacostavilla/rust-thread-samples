c4a9309c4214098d96665912bc7ae904e0328906
rafacostavilla <rafaelcostavilla@gmail.com>
2024-09-04 19:05:13 +1200

    Use thread channel

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use std::{thread};
+use std::{sync::mpsc, thread};
 
 fn main(){
-    let v = vec![1, 2, 3];
+    let (tx, rx) = mpsc::channel();
 
-    let handle = thread::spawn(move ||{
-        println!("Here's a vector: {:?}", v);
+    thread::spawn(move || {
+        let msg = String::from("hi");
+        tx.send(msg).unwrap();
     });
 
-    handle.join().unwrap();
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
 }
